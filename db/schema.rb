57caeac8aa3186b2c03443fd10cda9e67a6be053
Heath Gargit <heath@gargit.games>
2020-01-05 14:32:23 +1100

    Finished Toy App

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_015134) do
+ActiveRecord::Schema.define(version: 2020_01_05_025848) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
